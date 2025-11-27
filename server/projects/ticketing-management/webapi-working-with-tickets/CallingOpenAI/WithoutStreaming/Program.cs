deeaf4ca7e0836add96be7db7925fce2f20b71f5
Evan BOISSONNOT <evan@boissonnot.fr>
2025-11-27 11:59:13 +0100

    feat: 2 examples of calling open ai with streaming or not calling

---
+++ server/projects/ticketing-management/webapi-working-with-tickets/CallingOpenAI/WithoutStreaming/Program.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+using ModelContextProtocol;
+using ModelContextProtocol.Client;
+
+using OpenAI;
+using OpenAI.Responses;
+
+IConfigurationRoot config = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+string? model = config["ModelName"];
+string? key = config["OpenAIKey"];
+
+var openAiClient = new OpenAIClient(key).GetChatClient(model);
+using IChatClient chatClient = openAiClient.AsIChatClient();
+
+await using var mcpClient = await McpClient.CreateAsync(new StdioClientTransport(new()
+{
+    Name = "Ticketing",
+    Command = "ticketing-mcp",
+}));
+
+var tools = await mcpClient.ListToolsAsync().ConfigureAwait(false);
+
+Console.WriteLine("\nPrompt ?");
+string prompt = Console.ReadLine();
+var messages = new List<ChatMessage>
+    {
+        new ChatMessage(ChatRole.System, "You are a helpful assistant that summarizes support tickets."),
+        new ChatMessage(ChatRole.User, prompt)
+    };
+
+var response = await chatClient.GetResponseAsync(messages, new() { Tools = [.. tools.Cast<AITool>()] });
+
+var functionCalls = response.Messages
+    .SelectMany(u => u.Contents)
+    .OfType<FunctionCallContent>()
+    .ToList();
+
+if (functionCalls.Any())
+{
+    messages.AddRange(response.Messages);
+
+    foreach (var functionCall in functionCalls)
+    {
+        var toolResult = await mcpClient.CallToolAsync(functionCall.Name, (IReadOnlyDictionary<string, object?>?)functionCall.Arguments).ConfigureAwait(false);
+        messages.Add(new ChatMessage(ChatRole.Tool, [new FunctionResultContent(functionCall.CallId, toolResult)]));
+    }
+
+    Console.WriteLine("\n[Traitement de la réponse finale...]");
+    response = await chatClient.GetResponseAsync(messages, new() { Tools = [.. tools.Cast<AITool>()] });
+}
+
+foreach (var message in response.Messages)
+{
+    if (!string.IsNullOrEmpty(message.Text))
+    {
+        Console.WriteLine(message.Text);
+    }
+}
+
+
+
+//ChatResponse response = await client.GetResponseAsync(messages, new ChatOptions { MaxOutputTokens = 600, ToolMode = ChatToolMode.Auto, Tools = [.. tools.Cast<AITool>()] });
+
+//if (response.Messages.Count > 0)
+//{
+//    Console.WriteLine("Response:");
+//    foreach (var message in response.Messages)
+//    {
+//        foreach (var content in message.Contents)
+//        {
+//            if(content is FunctionCallContent toolCall)
+//            {
+//            }
+//        }
+//    }
+//}
---
+++ server/projects/ticketing-management/webapi-working-with-tickets/ConsoleAppToMcp/ConsoleAppToMcp/Program.cs
@@ -0,0 +1,24 @@
+﻿using ModelContextProtocol.Client;
+using ModelContextProtocol.Protocol;
+
+var clientTransport = new StdioClientTransport(new StdioClientTransportOptions
+{
+    Name = "Ticketing",
+    Command = "ticketing-mcp",
+});
+
+var client = await McpClient.CreateAsync(clientTransport);
+
+foreach (var tool in await client.ListToolsAsync())
+{
+    Console.WriteLine($"{tool.Name} ({tool.Description})");
+}
+
+var result = await client.CallToolAsync
+(
+    "get_tickets",
+    new Dictionary<string, object?>(),
+    cancellationToken: CancellationToken.None
+);
+
+Console.WriteLine(result.Content.OfType<TextContentBlock>().First().Text);
